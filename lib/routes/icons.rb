5b87470c97e421a1f7030151c8bccbdbdc1a0673
Johannes <jhedtrich@gmail.com>
2018-01-18 19:37:13 +0100

    redirect to icon from web-vault because of no cross-origin allowed, the requests fail in the web-app anyways.

--- lib/routes/icons.rb
+++ lib/routes/icons.rb
@@ -20,8 +20,8 @@ module BitwardenRuby
         app.namespace ICONS_URL do
           get "/:domain/icon.png" do
             # TODO: do this service ourselves
-
-            redirect "http://#{params[:domain]}/favicon.ico"
+            redirect url("images/fa-globe.png")
+            #redirect "http://#{params[:domain]}/favicon.ico"
           end
         end
       end
