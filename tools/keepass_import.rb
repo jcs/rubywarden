8e0ea1c0526ca4173f04e39b11472def93c30f8e
Martin Gross <martin@pc-coholic.de>
2018-03-12 15:44:02 +0100

    Adding license header, import cleanup

--- tools/keepass_import.rb
+++ tools/keepass_import.rb
@@ -1,6 +1,33 @@
+#!/usr/bin/env ruby
+#
+# Copyright (c) 2018 joshua stein <jcs@jcs.org>
+# Keepass importer by Martin Gross <martin@pc-coholic.de>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+#
+# Read a given Keepass kdbx file, optional keyfile, ask for the given user's
+# master password, then lookup the given user in the bitwarden-ruby SQLite
+# database and fetch its key. Each Keepass password entry is encrypted and
+# inserted into the database.
+#
+# No check is done to eliminate duplicates, so this is best used on a fresh
+# bitwarden-ruby installation after creating a new account.
+#
+
 require File.realpath(File.dirname(__FILE__) + "/../lib/bitwarden_ruby.rb")
 require "getoptlong"
-require "uri"
 require "rubeepass"
 
 def usage
@@ -114,7 +141,7 @@ def getEntries(db)
       cdata['Notes'] = encrypt(entry[1].notes) if entry[1].notes.present?
 
       if entry[1].attachments.any?
-        puts "This entry has an attachment - but it won't be converted as bitwarden_ruby does not support attachments."
+        puts "This entry has an attachment - but it won't be converted as bitwarden_ruby does not support attachments yet."
       end
 
       c.data = cdata.to_json
