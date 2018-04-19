a4726ef274a1e5a7d14eb54d049d84a586dd367a
Johannes / universa1 <jhedtrich@gmail.com>
2018-04-19 11:37:21 +0200

    migrate timestamps migrate only tables for which a model exists

--- tools/migrate_to_ar.rb
+++ tools/migrate_to_ar.rb
@@ -31,16 +31,19 @@ usage unless environment
 
 require 'yaml_db'
 require 'fileutils'
+require File.realpath(File.dirname(__FILE__) + "/../lib/bitwarden_ruby.rb")
+ActiveRecord::Base.remove_connection
 
 data_file = "db/dump.yml"
 
 dbconfig = YAML.load(File.read('db/config.yml'))
 ActiveRecord::Base.establish_connection dbconfig[environment]
 
-# hack to ignore schema_version table
+# select only tables for defined models
 class YamlDb::SerializationHelper::Dump
   def self.tables
-    ActiveRecord::Base.connection.tables.reject { |table| ['schema_info', 'schema_migrations', 'schema_version'].include?(table) }.sort
+    #ActiveRecord::Base.connection.tables.reject { |table| ['schema_info', 'schema_migrations', 'schema_version'].include?(table) }.sort
+    ObjectSpace.each_object(Class).select {|k| k < DBModel}.map {|k| k.table_name }
   end
 end
 
@@ -55,3 +58,12 @@ system "rake db:migrate RACK_ENV=#{environment}"
 ActiveRecord::Base.establish_connection dbconfig[environment]
 YamlDb::SerializationHelper::Base.new(YamlDb::Helper).load(data_file)
 FileUtils.rm data_file
+
+# reset created_at / updated_at from seconds since epoch to actual datetime for ar magic
+DBModel.record_timestamps = false
+ObjectSpace.each_object(Class).select {|k| k < DBModel}.each do |k|
+  k.all.each do |i|
+    i.update created_at: Time.at(i.created_at), updated_at: Time.at(i.updated_at)
+  end
+end
+DBModel.record_timestamps = true
