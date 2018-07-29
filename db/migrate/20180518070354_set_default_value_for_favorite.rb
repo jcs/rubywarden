17be03426b53ba478804142d6eb47f44895c5aea
Johannes <jhedtrich@gmail.com>
2018-07-29 11:13:51 +0200

    do not allow null value for favorite

--- db/migrate/20180518070354_set_default_value_for_favorite.rb
+++ db/migrate/20180518070354_set_default_value_for_favorite.rb
@@ -2,6 +2,6 @@ class SetDefaultValueForFavorite < ActiveRecord::Migration[5.1]
   class Cipher < ActiveRecord::Base; end
   def change
     change_column_default :ciphers, :favorite, false
-    Cipher.where(favorite: nil).update_all favorite: false
+    change_column_null(:ciphers, :favorite, false, false)
   end
 end
