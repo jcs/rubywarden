c55f926548abc25e763bfcab862e1dbfa8df201f
joshua stein <jcs@jcs.org>
2019-07-24 22:21:30 -0500

    Fix 3c5a7ca65
    Set null kdf_iterations to 5000, which is what the old default used
    to be.  As the default changes in the future (or as it did in
    5ccd4174b), this should still be 5000.

--- db/migrate/20190724163354_no_null_kdf_iterations.rb
+++ db/migrate/20190724163354_no_null_kdf_iterations.rb
@@ -1,15 +1,17 @@
 class NoNullKdfIterations < ActiveRecord::Migration[5.1]
   def change
     User.all.each do |u|
+      # any old users without a kdf_iterations value probably have the old
+      # value of 5000
       if !u.kdf_iterations
-        u.kdf_iterations = Bitwarden::KDF::DEFAULT_ITERATIONS[User::DEFAULT_KDF_TYPE]
-      end
-      if !u.kdf_type
-        u.kdf_type = User::DEFAULT_KDF_TYPE
+        u.kdf_iterations = 5000
+        u.kdf_type = Bitwarden::KDF::PBKDF2
       end
       u.save!
     end
 
+    # but going forward, any new users should get whatever defaults are set in
+    # the future
     change_column :users, :kdf_iterations, :integer, :null => false,
       :default => Bitwarden::KDF::DEFAULT_ITERATIONS[User::DEFAULT_KDF_TYPE]
   end
